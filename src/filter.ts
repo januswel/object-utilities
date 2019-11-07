a4d309e2d079a0cd48d1356305b7eb162e03fed3
Takagi Kensuke <janus.wel.3@gmail.com>
2019-11-07 10:06:07 +0900

    :art: rename

--- src/filter.ts
+++ src/filter.ts
@@ -1,10 +1,10 @@
-export type Filterer<V> = (key: string, value: V) => boolean
+export type Predicate<V> = (key: string, value: V) => boolean
 
 type ValueOf<T> = T[keyof T]
 
-export default function filter<S extends object, V extends ValueOf<S>>(src: S, filterer: Filterer<V>) {
+export default function filter<S extends object, V extends ValueOf<S>>(src: S, predicate: Predicate<V>) {
   return Object.entries(src).reduce((result, [key, value]) => {
-    if (filterer(key, value)) {
+    if (predicate(key, value)) {
       result[key] = value
     }
     return result
