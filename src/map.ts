fb882cf7ae6fe4160a27e83d9d888a845e36cb65
Takagi Kensuke <janus.wel.3@gmail.com>
2019-11-13 20:08:44 +0900

    :heavy_plus_sign::wrench::rotating_light: use ESLint

--- src/map.ts
+++ src/map.ts
@@ -2,7 +2,7 @@ export type Mapper<V, R> = (key: string, value: V) => R
 
 type ValueOf<T> = T[keyof T]
 
-export default function map<S extends object, V extends ValueOf<S>>(src: S, mapper: Mapper<V, any>) {
+export default function map<S extends object, V extends ValueOf<S>, R>(src: S, mapper: Mapper<V, R>) {
   return Object.entries(src).reduce((result, [key, value]) => {
     result[key] = mapper(key, value)
     return result
